d768ce5057a9fbe56beab24b0b8b282dc7262f5e
LeVraiGG <tux.caroline@gmail.com>
2023-12-21 10:34:45 +0100

    jdbc updated

--- src/server/REST_API/src/main/java/rest/api/helpers/PostgesqlJDBC.java
+++ src/server/REST_API/src/main/java/rest/api/helpers/PostgesqlJDBC.java
@@ -1,7 +1,57 @@
-package rest.api.helpers;
+package bdr.projet.helpers;
 
 import java.sql.*;
 import java.util.Properties;
+
+/**
+ *
+ *private void fetchAll() {
+ *         fetchGames();
+ *         fetchMods();
+ *     }
+ *
+ *     private void fetchGames() {
+ *         if(!needToFetchGames) return;
+ *
+ *         try {
+ *             PreparedStatement request = jdbc.getPrepareStatement(DB_RQ_GET_GAMES);
+ *             ResultSet rs = jdbc.R(request);
+ *
+ *             while (rs.next()) {
+ *                 String gameName = rs.getString(1);
+ *                 if (getGame(gameName) != null) continue; //TODO OR UPDATE
+ *                 games.add(new Game(gameName));
+ *             }
+ *
+ *             rs.close();
+ *             request.close();
+ *             needToFetchGames = false;
+ *         } catch (SQLException ignored) {
+ *         }
+ *     }
+ *
+ *     private void fetchMods() {
+ *         //Test function. TODO mod class and changes this function next
+ *         if(!needToFetchMods) return;
+ *         fetchGames();
+ *
+ *         try {
+ *             PreparedStatement request = jdbc.getPrepareStatement(DB_RQ_GET_MODS);
+ *             ResultSet rs = jdbc.R(request);
+ *
+ *             while (rs.next()) { //TODO DEBUG
+ *                 mods.add(new Mod(rs.getString(2), getGame(rs.getString(1))));
+ *             }
+ *
+ *             rs.close();
+ *             request.close();
+ *             needToFetchMods = false;
+ *         } catch (SQLException ignored) {
+ *         }
+ *     }
+ *
+ *
+ */
 
 public class PostgesqlJDBC {
 
@@ -21,17 +71,41 @@ public class PostgesqlJDBC {
         db = DriverManager.getConnection(url, props);
     }
 
-    public void doRequest(String request) throws SQLException {
-        int foovalue = 500;
-        PreparedStatement st = db.prepareStatement(request);
-        ResultSet rs = st.executeQuery();
-        while (rs.next()) {
+    public boolean isConnect(){
+        return db != null;
+    }
 
-         //TODO
-            System.out.print("Column 1 returned ");
+    public PreparedStatement getPrepareStatement(String request) throws SQLException { //could do variable parameter but would complexity the code for nothing
+        return db.prepareStatement(request);
+    }
+
+    public ResultSet R(PreparedStatement request) throws SQLException {
+        ResultSet rs = request.executeQuery();
+
+        /*while (rs.next()) {
+            System.out.print("Column 1 returned "); //TODO RESOLVE THAT ISSUE
             System.out.println(rs.getString(1));
         }
+
         rs.close();
-        st.close();
+        request.close();*/
+        return  rs;
+    }
+
+    public int CUD(PreparedStatement request) throws SQLException {
+        int res = request.executeUpdate();
+        request.close();
+        return res;
+    }
+
+    public boolean LDD(PreparedStatement request) throws SQLException {
+        boolean res = request.execute();
+        request.close();
+        return res;
+    }
+
+    public void disconnect() throws SQLException {
+        db.close();
+        db = null;
     }
 }
